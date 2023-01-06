ecd1f798618210c087e042b923023b2c99d87060
Sandip Mondal <sandipmondal00@gmail.com>
2023-01-06 21:28:01 +0530

    new route added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import NewQuote from "./pages/NewQuote";
 import QuoteDetail from "./pages/QuoteDetail";
 import NotFound from "./pages/NotFound";
 import Layout from "./components/layout/Layout";
+import Comments from "./components/comments/Comments";
 
 function App() {
   return (
@@ -11,8 +12,9 @@ function App() {
     <Switch>
       <Route path='/' exact > <Redirect  to='/quotes' />   </Route>
       <Route path='/quotes' exact>   <AllQuotes/>    </Route>   //This path will render list of quotes
-      <Route  path='/quotes/:quoteId'> <QuoteDetail/> </Route>  //To render a details page of a particular quote
+      <Route  path='/quotes/:quoteId' > <QuoteDetail/> </Route>  //To render a details page of a particular quote
       <Route  path='/new-quote'><NewQuote/> </Route>  //To render a new quote
+      {/* <Route path='/quotes/:quoteId/comments'> <Comments/> </Route> */}
       <Route path='*'> <NotFound/> </Route>
 
 
--- src/components/layout/MainNavigation.js
+++ src/components/layout/MainNavigation.js
@@ -1,12 +1,21 @@
 import React from 'react'
-import { NavLink } from 'react-router-dom'
+import { NavLink, useRouteMatch } from 'react-router-dom'
 import classes from './MainNavigation.module.css'
 
+
 const MainNavigation = () => {
+  const match= useRouteMatch()
+
   return (
     
         <header className={classes.header}>
-            <h2 className={classes.logo}>Great Quotes</h2>
+          <ul>
+            <li>
+            <NavLink activeClassName={classes.active}  to={match.path}><h2 className={classes.logo}>    Great Quotes</h2></NavLink>
+
+            </li>
+          </ul>
+            
             <nav className={classes.nav}>
 
 
